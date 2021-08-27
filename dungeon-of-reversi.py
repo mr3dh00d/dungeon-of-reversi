b1b6095ebd32d0a4ad76d3fc973b0aafe2017e97
Dereck Pavez <dereck.pavez@outlook.com>
2021-08-26 22:27:10 -0400

    add background

--- dungeon-of-reversi.py
+++ dungeon-of-reversi.py
@@ -1,15 +1,12 @@
 import cocos
-
-class HelloCocos(cocos.layer.Layer):
-    def __init__(self):
-        super().__init__()
-        label = cocos.text.Label("Hello Cocos", font_name="CaskaydiaCove Nerd Font", font_size=32,
-                                anchor_x="center", anchor_y="center")
-        label.position = 450, 360
-        self.add(label)
+import modules.background as background
+from cocos.director import director
 
 if __name__=="__main__":
-    cocos.director.director.init(width=900, height=675, caption="Dungeon of Reversi")
-    hello_layer=HelloCocos()
-    test_scene =  cocos.scene.Scene(hello_layer)
-    cocos.director.director.run(test_scene)
+    director.init(width=900, height=900, caption="Dungeon of Reversi")
+    bkg = background.Background()
+    # bkg.setCorupted()
+    bkg.setPosible()
+    bkg.setBlock()
+    test_scene =  cocos.scene.Scene(bkg)
+    director.run(test_scene)
---
+++ modules/background.py
@@ -0,0 +1,32 @@
+import cocos
+import pyglet
+from cocos.director import director
+
+class Background(cocos.layer.Layer):
+
+    def __init__(self):
+        super().__init__()
+        self.backgorund_imagen=pyglet.image.ImageGrid(pyglet.image.load("assets/sprites/scenes/center/center.png"),1,4)
+        self.setBlock()
+
+
+    def setBlock(self):
+        self.__set(0)
+
+    def setPosible(self):
+        self.__set(1)
+        
+    def setSelected(self):
+        self.__set(2)
+        
+    def setCorupted(self):
+        self.__set(3)
+
+    def __set(self, bkg_img):
+        try:
+            self.remove(self.spr)
+        except AttributeError:
+            pass
+        self.spr = cocos.sprite.Sprite(self.backgorund_imagen[bkg_img], scale=0.8)
+        self.spr.position = director.get_window_size()[0]/2, director.get_window_size()[1]/2
+        self.add(self.spr)
